7b2a1e111b7abfb0abc0a68b9e8eb52bb72ce70b
Romain N <insc.romain@gmail.com>
2022-01-24 22:17:22 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import data from "./data.js";
+import "./App.css";
 
 function App() {
+  const [highScore, setHighScore] = useState(0);
+  const [currentGameList, setCurrentGameList] = useState(data);
+  const [gameOver, setGameOver] = useState(false);
+
+  useEffect(()=>{
+    shuffleImages(data)
+  },[currentGameList])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setGameOver(false);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [gameOver]);
+
+  const handleClick = (id) => {
+    const checkObject = currentGameList.find((item) => item.id === id);
+    if (checkObject) {
+      const newGameList = currentGameList.filter((item) => item.id !== id);
+      setCurrentGameList(newGameList);
+    } else {
+      setGameOver(true);
+      const currentScore = data.length - currentGameList.length;
+      if (currentScore > highScore) {
+        setHighScore(currentScore);
+      }
+      setCurrentGameList(data);
+    }
+  };
+
+  const shuffleImages = (array) => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main>
+      <h1>Memory Game</h1>
+      {gameOver && (
+        <p className="game-over">
+          <span className="green">Game Over</span> - Score has been reset!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      )}
+      <header>
+        <h2>
+          game score:{" "}
+          <span className="green">
+            {data.length - currentGameList.length}
+          </span>
+        </h2>
+        <h4>
+          Highest Score: <span className="green">{highScore}</span>
+        </h4>
       </header>
-    </div>
+      <div className="main-center">
+        {data.map((fruit) => {
+          const { id, image, name } = fruit;
+          return (
+            <article key={id}>
+              <div className="img-container">
+                <img
+                  src={image}
+                  alt={name}
+                  className="fruit-img"
+                  onClick={() => handleClick(id)}
+                />
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,16 @@
+import uniqid from "uniqid";
+
+const data = [
+  { name: "banana", image: "/img/banana.png", id: uniqid() },
+  { name: "dragon", image: "./img/dragon.png", id: uniqid() },
+  { name: "kiwi", image: "./img/kiwi.png", id: uniqid() },
+  { name: "lichi", image: "./img/lichi.png", id: uniqid() },
+  { name: "lime", image: "./img/lime.png", id: uniqid() },
+  { name: "mango", image: "./img/mango.png", id: uniqid() },
+  { name: "orange", image: "./img/orange.png", id: uniqid() },
+  { name: "papaya", image: "./img/papaya.png", id: uniqid() },
+  { name: "pineapple", image: "./img/pineapple.png", id: uniqid() },
+  { name: "pomegranate", image: "./img/pomegranate.png", id: uniqid() },
+];
+
+export default data;
